342d7964f97aa5c3b1e465e6f2a4768bb05286df
pkumar5 <pawan.kumar2@intsof.com>
2017-10-05 15:23:25 +0530

    [PK]:add new method

--- DemoApplication/DemoApplication/Default.aspx.cs
+++ DemoApplication/DemoApplication/Default.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,9 +11,21 @@ namespace DemoApplication
 {
     public partial class Default : System.Web.UI.Page
     {
+        
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        public string createFolder()
+        {
+            string folderlocation = "C://" + DateTime.Now.ToString("MMddyyyy") ;
+
+            if (!Directory.Exists(folderlocation))
+            {
+                // Try to create the directory.
+                Directory.CreateDirectory(folderlocation);
+            }
+            return folderlocation;
         }
     }
 }
